e8cb975082debb61de9125b78b6ca91689b15896
thomlund <t.lundgaard@outlook.dk>
2025-09-02 20:27:40 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 let dist = 0
+let foreivet = 0
+IR.IR_callbackUser(function (foreivet) {
+	
+})
 basic.forever(function () {
     dist = maqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14)
     if (dist >= 10) {
         maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 40)
     } else {
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 0)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 80)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Backward, 80)
     }
+    foreivet = 0
 })
